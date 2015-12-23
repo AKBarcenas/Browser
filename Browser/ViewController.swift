2de2556e5093c900dad7487af14d6fd8228e4902
AKBarcenas <akbarcen@ucsd.edu>
2015-12-23 01:59:00 -0800

    User can open webpages now

--- Browser/ViewController.swift
+++ Browser/ViewController.swift
@@ -24,6 +24,24 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let url = NSURL(string: "https://www.hackingwithswift.com")!
         webView.loadRequest(NSURLRequest(URL: url))
         webView.allowsBackForwardNavigationGestures = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .Plain, target: self, action: "openTapped")
+    }
+    
+    func openTapped() {
+        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .ActionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .Default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .Default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
+    }
+    
+    func openPage(action: UIAlertAction!) {
+        let url = NSURL(string: "https://" + action.title!)!
+        webView.loadRequest(NSURLRequest(URL: url))
+    }
+    
+    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+        title = webView.title
     }
 
     override func didReceiveMemoryWarning() {
